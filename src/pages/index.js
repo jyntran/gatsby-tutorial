803a086fd91d6b258ab67f1aa891307a595fbb26
J. Tran <jyntran@gmail.com>
2017-10-31 13:43:24 -0500

    Set up page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
+import Link from "gatsby-link"
 
-export default () => <div>Hello world!</div>
+export default () =>
+	<div style={{ color: 'tomato' }}>
+		<h1>Hello Gatsby!</h1>
+		<p>What a world.</p>
+		<img src="http://lorempixel.com/400/200/" alt="" />
+		<br />
+		<Link to="/page-2/">Link</Link> 
+		<Link to="/counter">Counter</Link>
+	</div>
